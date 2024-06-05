5643ebe0622d69490df6b90777cf2d93ed5d4703
NguyenCong04 <nguyentatcong2004@gmail.com>
2024-06-05 17:27:14 +0700

    Initial commit

---
+++ app/src/main/java/com/congntph34559/fpoly/reviewroomdatabaseapplication/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.congntph34559.fpoly.reviewroomdatabaseapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GetLayoutAppNavigation()
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+
+}
